5d56d221ea21ea32732122554a38ecf620070b92
baesisi3648 <baesisi3648@gmail.com>
2025-06-30 19:14:11 +0900

    6/30 입력3

---
+++ SQL/codes/01-createdb.sql
@@ -0,0 +1,17 @@
+-- DB 생성
+CREATE DATABASE sample_db;
+
+-- DB 확인
+SHOW DATABASES;
+
+-- DB 삭제
+DROP DATABASE sample_db;
+
+-- lecture, practice 생성후 확인
+CREATE DATABASE lecture;
+CREATE DATABASE practice;
+
+SHOW DATABASES;
+
+-- DB 사용
+USE lecture;
